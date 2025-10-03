ea9d5b1186d3b2f43eab2d52a65d884d496cee86
YAKSHMEHTA <yakshvardhansinghmehta@gamil.com>
2025-10-03 18:05:21 +0530

    Added index.html

---
+++ practice/vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import KgButton from "./KgButton";
+
+function App() {
+  return (
+    <div>
+      <h1>yaksh vardhan singh mehta</h1>
+      <KgButton />
+    </div>
+  )
+}
+
+export default App;
